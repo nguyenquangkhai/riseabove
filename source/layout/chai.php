36f5e518b83e75fd337f5858b52c4e1bf2e18a45
RedFx <redfox273@gmail.com>
2013-01-10 18:19:32 +0700

    Gallery

--- source/layout/chai.php
+++ source/layout/chai.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'chai_config.php';
-
-function create_chai($chai, $template, $topic, $image1, $image2, $text1, $text2, $uid) {
+include("database.php");
+function create_chai($template, $topic, $image1, $image2, $text1, $text2, $uid) {
 	//Load chai
 	if ($uid != null) {
 		$chai = imagecreatefrompng('images/bottle.png');
@@ -11,35 +11,46 @@ function create_chai($chai, $template, $topic, $image1, $image2, $text1, $text2,
 		//Load image
 		$image_1 = imagecreatefrompng('images/creative/images/' . substr($image1, 0, 1) . '/' . $image1 . '.png');
 		$image_2 = imagecreatefrompng('images/creative/images/' . substr($image2, 0, 1) . '/' . $image2 . '.png');
-		$text_1 = strip_tags($text1);
-		$text_2 = strip_tags($text2);
+		$text_1 = trim(strip_tags($text1));
+		$text_2 = trim(strip_tags($text2));
 		switch ($template) {
+			
 			case 1 :
-				imagecopy($chai, $image_1, ($chai_w - imagesx($image_1)) / 2, CONTENT_Y, 0, 0, imagesx($image_1), imagesy($image_1));
-				imagettftext($chai, TEXT_SIZE, 0, ($chai_w - TEXT_WIDTH) / 2, CONTENT_Y + imagesy($image_1) + 15, TEXT_COLOR, TEXT_FONT, wordwrap($text_1, TEXT_MAX_CHAR));
+				if($image1 != 0)
+					imagecopy($chai, $image_1, ($chai_w - imagesx($image_1)) / 2, CONTENT_Y, 0, 0, imagesx($image_1), imagesy($image_1));
+				if($text1 != "")
+					imagettftext($chai, TEXT_SIZE, 0, ($chai_w - TEXT_WIDTH) / 2, CONTENT_Y + imagesy($image_1) + 15, TEXT_COLOR, TEXT_FONT, wordwrap($text_1, TEXT_MAX_CHAR));
 				break;
 			case 2 :
-				imagettftext($chai, TEXT_SIZE, 0, ($chai_w - TEXT_WIDTH) / 2, CONTENT_Y, TEXT_COLOR, TEXT_FONT, wordwrap($text_1, TEXT_MAX_CHAR));
-				imagecopy($chai, $image_1, ($chai_w - imagesx($image_1)) / 2, CONTENT_Y + TEXT_HEIGHT_NORMAL + 15, 0, 0, imagesx($image_1), imagesy($image_1));
+				if($text1 != "")
+					imagettftext($chai, TEXT_SIZE, 0, ($chai_w - TEXT_WIDTH) / 2, CONTENT_Y, TEXT_COLOR, TEXT_FONT, wordwrap($text_1, TEXT_MAX_CHAR));
+				if($image1 != 0)
+					imagecopy($chai, $image_1, ($chai_w - imagesx($image_1)) / 2, CONTENT_Y + TEXT_HEIGHT_NORMAL + 15, 0, 0, imagesx($image_1), imagesy($image_1));
 				break;
 			case 3 :
-				imagettftext($chai, TEXT_SIZE, 0, ($chai_w - TEXT_WIDTH) / 2, CONTENT_Y, TEXT_COLOR, TEXT_FONT, wordwrap($text_1, TEXT_MAX_CHAR));
-				imagecopy($chai, $image_1, ($chai_w - imagesx($image_1)) / 2, CONTENT_Y + TEXT_HEIGHT_SMALL + 15, 0, 0, imagesx($image_1), imagesy($image_1));
-				imagettftext($chai, TEXT_SIZE, 0, ($chai_w - TEXT_WIDTH) / 2, CONTENT_Y + TEXT_HEIGHT_SMALL + imagesy($image_1) + 30, TEXT_COLOR, TEXT_FONT, wordwrap($text_2, TEXT_MAX_CHAR));
+				if($text1 != "")
+					imagettftext($chai, TEXT_SIZE, 0, ($chai_w - TEXT_WIDTH) / 2, CONTENT_Y, TEXT_COLOR, TEXT_FONT, wordwrap($text_1, TEXT_MAX_CHAR));
+				if($image1 != 0)
+					imagecopy($chai, $image_1, ($chai_w - imagesx($image_1)) / 2, CONTENT_Y + TEXT_HEIGHT_SMALL + 15, 0, 0, imagesx($image_1), imagesy($image_1));
+				if($text2 != "")
+					imagettftext($chai, TEXT_SIZE, 0, ($chai_w - TEXT_WIDTH) / 2, CONTENT_Y + TEXT_HEIGHT_SMALL + imagesy($image_1) + 30, TEXT_COLOR, TEXT_FONT, wordwrap($text_2, TEXT_MAX_CHAR));
 				break;
 			case 4 :
 				$wid = imagesx($image_1) * IMAGE_HEIGHT_SMALL / imagesy($image_1);
-				imagecopy($chai, $image_1, ($chai_w - imagesx($image_1)) / 2, CONTENT_Y, 0, 0, imagesx($image_1), imagesy($image_1));
-				//imagecopyresized($chai, $image_1, ($chai_w - $wid) / 2, CONTENT_Y, 0, 0, $wid, IMAGE_HEIGHT_SMALL, imagesx($image_1), imagesy($image_1));
-				$text_hold = imagettftext($chai, TEXT_SIZE, 0, ($chai_w - TEXT_WIDTH) / 2, CONTENT_Y + imagesy($image_1) + 15, TEXT_COLOR, TEXT_FONT, wordwrap($text_1, TEXT_MAX_CHAR));
-				//imagecopyresized($chai, $image_2, ($chai_w - $wid) / 2, CONTENT_Y + IMAGE_HEIGHT_SMALL +TEXT_HEIGHT_NORMAL + 15, 0, 0,  $wid, IMAGE_HEIGHT_SMALL, imagesx($image_1), imagesy($image_1));
-				imagecopy($chai, $image_2, ($chai_w - imagesx($image_2)) / 2, CONTENT_Y + imagesy($image_1) + $text_hold[6] + 15, 0, 0, imagesx($image_2), imagesy($image_2));
+				if($image1 != 0)
+					imagecopy($chai, $image_1, ($chai_w - imagesx($image_1)) / 2, CONTENT_Y, 0, 0, imagesx($image_1), imagesy($image_1));
+				if($text1 != "")
+					$text_hold = imagettftext($chai, TEXT_SIZE, 0, ($chai_w - TEXT_WIDTH) / 2, CONTENT_Y + imagesy($image_1) + 15, TEXT_COLOR, TEXT_FONT, wordwrap($text_1, TEXT_MAX_CHAR));
+				if($image2 != 0)
+					imagecopy($chai, $image_2, ($chai_w - imagesx($image_2)) / 2, CONTENT_Y + imagesy($image_1) + $text_hold[6] + 15, 0, 0, imagesx($image_2), imagesy($image_2));
 				break;
 			case 5 :
-				imagettftext($chai, TEXT_SIZE, 0, ($chai_w - TEXT_WIDTH) / 2, CONTENT_Y, TEXT_COLOR, TEXT_FONT, wordwrap($text_1, TEXT_MAX_CHAR));
+				if($text1 != "")
+					imagettftext($chai, TEXT_SIZE, 0, ($chai_w - TEXT_WIDTH) / 2, CONTENT_Y, TEXT_COLOR, TEXT_FONT, wordwrap($text_1, TEXT_MAX_CHAR));
 				break;
 			case 6 :
-				imagecopy($chai, $image_1, ($chai_w - imagesx($image_1)) / 2, CONTENT_Y, 0, 0, imagesx($image_1), imagesy($image_1));
+				if($image1 != 0)
+					imagecopy($chai, $image_1, ($chai_w - imagesx($image_1)) / 2, CONTENT_Y, 0, 0, imagesx($image_1), imagesy($image_1));
 				break;
 			default :
 				break;
@@ -49,7 +60,10 @@ function create_chai($chai, $template, $topic, $image1, $image2, $text1, $text2,
 		imagealphablending($thumb, false);
 		imagesavealpha($thumb, true);
 		imagecopyresampled($thumb, $chai, 0, 0, 0, 0, imagesx($chai), imagesy($chai), imagesx($chai), imagesy($chai));
-		$result = imagepng($thumb, GALLERY_URL.GALLERY_PATTERN. $uid . ".png");
+		$gallery_id = GALLERY_URL.GALLERY_PATTERN.$uid.".png";
+		$result = imagepng($thumb,  $gallery_id);
+		$insert_gallery_query = "INSERT INTO gallery_image (name_image) VALUES ('$gallery_id')";
+		mysql_query($insert_user_info_query, $con);
 		imagedestroy($thumb);
 		return $result;
 	} else {
@@ -57,6 +71,10 @@ function create_chai($chai, $template, $topic, $image1, $image2, $text1, $text2,
 	}
 }
 
-
-$result_image = create_chai($chai, 4, 3, 1001, 2001, $text, $text2, 123);
+$user_list = mysql_query("SELECT * FROM user_info");
+while($row = mysql_fetch_array($user_list)){
+	if($row[template_id] != NULL)
+		create_chai($row[template_id], $row[topic_id], $row[image_1_id], $row[image_2_id], $row[text_1], $row[text_2], $row[user_info_id]);
+}
+ 
 //header('Content-type: text/html; charset=utf-8');
