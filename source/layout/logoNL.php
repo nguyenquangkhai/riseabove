df094388fcd847ee98b700adda7309a83b9360e8
quang <herokes@yahoo.com>
2013-01-17 19:24:23 +0700

    fix some bug logo

--- source/layout/logoNL.php
+++ source/layout/logoNL.php
@@ -1,7 +1,7 @@
 ﻿<style>
-.logo-link{float:left;}
+.logo-link{float:left; display: block;}
 	#logoNL{width: 100px;}
 </style>
-<a class="logo-link" href="https://www.nganluong.vn/riseabove.vn">
+<a class="logo-link" href="https://www.nganluong.vn/riseabove.vn" target="_blank">
 	<img id="logoNL" src="images/NL logo.gif" alt="" />
 </a>
