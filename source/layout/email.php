e6638399dfa357e0c0a85abf58ca6f161158b217
ho dac vinh quang <herokes@yahoo.com>
2013-01-23 16:42:28 +0700

    email page

---
+++ source/layout/email.php
@@ -0,0 +1,25 @@
+﻿<? include('config.php'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta name="description" content="Website cung cấp quà Tết 2013 và Valentine 2013 dành cho sản phẩm rượu Martell VSOP. Sản phẩm rượu cognac nổi tiếng từ Pháp.Website Risabove">
+	<meta name="keywords" content="Martell, Martell VSOP, Riseabove, rượu cognac, VSOP, quà xuân, quà tết quà Valentine 2013. Quà độc đáo, quà handmade, quà tặng Tết Nguyên Đán, Túi quà">
+    <title><?=PAGE_TITLE?></title>
+	<link href="<?=FALVICON_PATH?>" rel="shortcut icon"/> 
+    <style type="text/css">
+        body{
+            background:url('http://i648.photobucket.com/albums/uu205/ankzu1989/edm8_zps51ab05c8.png') no-repeat top center;
+            margin:0;
+            padding:0;
+        }
+        div.wrapper{margin:0 auto;width:680px;height:1050px;}
+        div.wrapper a{display:block;width:680px;height:1050px;}
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+         <a target="_blank"  href="http://quatang.riseabove.vn/"></a>
+    </div>
+</body>
+</html>
